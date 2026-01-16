38a6fbcd7c8fdd770a67ff4780c2002994de9f8a
Gaga Demetrashvili <gagademetrashvili@credo.ge>
2026-01-16 17:57:32 +0400

    Polish code

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -7,8 +7,8 @@ export function useRecentStays() {
     const [searchParams] = useSearchParams();
 
     const numDays = !searchParams.get('last') ? 7 : Number(searchParams.get('last'));
-    const queryDate = subDays(new Date(), numDays).
-        toISOString();
+    const queryDate = subDays(new Date(), numDays)
+        .toISOString();
 
     const { isLoading, data: stays } = useQuery({
         queryFn: () => getStaysAfterDate(queryDate),
